386824dd377d67c8e20cec948e0209c92ed7896d
ronaldsg20 <roasarmientoga@unal.edu.co>
2019-07-02 17:02:07 -0500

    run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+make
+sudo rm -R /home/mpiuser/paralela-practica-4/
+cd ..
+sudo cp -R paralela-practica-4 /home/mpiuser/
+sudo chown -R mpiuser:mpiuser /home/mpiuser/paralela-practica-4
+cd /home/mpiuser/paralela-practica-4
