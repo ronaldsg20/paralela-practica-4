1fc32dd31a75b0c9eb28fb3ba1a2a300b83a743d
ronaldsg20 <roasarmientoga@unal.edu.co>
2019-07-07 23:03:08 -0500

    add resultados.txt

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -1,5 +1,5 @@
 
-echo "" > resultados.txt
+echo "" >> resultados.txt
 
 echo "--------Imagen 720px--------" >> resultados.txt
 for hilo in 1 2 3 4 5 6 7 8 9 10;
@@ -7,7 +7,7 @@ for hilo in 1 2 3 4 5 6 7 8 9 10;
         for kernel in 4 6 8 10 12 14;
         do
             echo "-------Kernel: $kernel ------ No de Hilos: $hilo ---------" >> resultados.txt
-            { time ./blur-effect 720-image.jpg 720-image-out.jpg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
+            { time mpirun -np 5 blur-effect 720-image.jpg 720-image-out.jpg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
         done
     done
 echo "--------Imagen 1080px--------" >> resultados.txt
@@ -16,7 +16,7 @@ for hilo in 1 2 3 4 5 6 7 8 9 10;
         for kernel in 4 6 8 10 12 14;
         do
             echo "-------Kernel: $kernel ------ No de Hilos: $hilo ---------" >> resultados.txt
-            { time ./blur-effect 1080-image.jpeg 1080-image-out.jpeg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
+            { time mpirun -np 5 blur-effect 1080-image.jpeg 1080-image-out.jpeg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
         done
     done
 echo "--------Imagen 4K--------" >> resultados.txt
@@ -25,7 +25,7 @@ for hilo in 1 2 3 4 5 6 7 8 9 10;
         for kernel in 4 6 8 10 12 14;
         do
             echo "-------Kernel: $kernel ------ No de Hilos: $hilo ---------" >> resultados.txt
-            { time ./blur-effect 4k-image.jpg 4k-image-out.jpg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
+            { time mpirun -np 5 blur-effect 4k-image.jpg 4k-image-out.jpg $kernel $hilo >/dev/null 2>&1;} |&  tee -a resultados.txt
         done
     done
 
